d5a4abccc237afaf2822d65a8084d11b46a95230
EagleDevs <58715617+Codyshep@users.noreply.github.com>
2024-03-24 11:16:07 -0500

    Add files via upload

---
+++ dailyreward/client/client.lua
@@ -0,0 +1,31 @@
+local isNuiOpen = false
+
+RegisterNetEvent('DailyNotify')
+AddEventHandler('DailyNotify', function(text, ntype)
+    local sourceId = sourceId
+    print(text, ntype)
+    exports['notifications']:sendnotify(text, ntype, 6000)
+end)
+
+RegisterCommand("daily", function()
+    isNuiOpen = not isNuiOpen
+    SetNuiFocus(isNuiOpen, isNuiOpen)
+    SendNUIMessage({
+        type = "toggleNui",
+        isOpen = isNuiOpen
+    })
+end, false)
+
+RegisterNUICallback('SetUIFocus', function(data, cb)
+    if isNuiOpen ~= false then
+        isNuiOpen = false
+    end
+    SetNuiFocus(isNuiOpen, isNuiOpen)
+    cb({})
+end)
+
+RegisterNUICallback('ClaimDaily', function(data, cb)
+    local source = data.sourceId
+    TriggerServerEvent('claimDaily', source)
+    cb({})
+end)
---
+++ dailyreward/config.lua
@@ -0,0 +1,20 @@
+config.rewarditems = {
+    {
+        item = 'money',
+        quantity = 5000
+    },
+    {
+        item = 'ammo-9',
+        quantity = 50
+    },
+    {
+        item = 'money',
+        quantity = 15000
+    },
+    {
+        item = 'money',
+        quantity = 25000
+    }
+}
+
+return config 
